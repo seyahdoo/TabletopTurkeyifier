4bde056c836c92fd4da77719bfa667fe0c66defc
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2018-08-18 22:16:39 +0300

    Refactored, Further Functionalized

--- main.py
+++ main.py
@@ -3,21 +3,6 @@ import os
 import shutil
 
 
-def inplace_change(file_path, old_string, new_string):
-    # Safely read the input filename using 'with'
-    with open(file_path, 'r', encoding='utf8', errors='ignore') as f:
-        s = f.read()
-        if old_string not in s:
-            # print('"{old_string}" not found in {file_path}.'.format(**locals()))
-            return
-
-    # Safely write the changed content, if found in the file
-    with open(file_path, 'w', encoding='utf8') as f:
-        # print('Changing "{old_string}" to "{new_string}" in {file_path}'.format(**locals()))
-        s = s.replace(old_string, new_string)
-        f.write(s)
-
-
 def get_path():
     path = None
 
@@ -33,61 +18,47 @@ def get_path():
 
 
 def do_backup(path):
-    if not os.path.isdir(path + "/Mods/WorkshopBACKUP"):
-        print("No Backup Found: Backing up to -> " + path + "/Mods/WorkshopBACKUP")
-        shutil.copytree(path + "/Mods/Workshop", path + "/Mods/WorkshopBACKUP")
+    if not os.path.isdir(path + "BACKUP"):
+        print("No Backup Found: Backing up to -> " + path + "BACKUP")
+        shutil.copytree(path, path + "BACKUP")
 
-    if not os.path.isdir(path + "/SavesBACKUP"):
-        print("No Backup Found: Backing up to -> " + path + "/SavesBACKUP")
-        shutil.copytree(path + "/Saves", path + "/SavesBACKUP")
+
+def inplace_change(file_path, old_string, new_string):
+    # Safely read the input filename using 'with'
+    with open(file_path, 'r', encoding='utf8', errors='ignore') as f:
+        s = f.read()
+        if old_string not in s:
+            # print('"{old_string}" not found in {file_path}.'.format(**locals()))
+            return
+
+    # Safely write the changed content, if found in the file
+    with open(file_path, 'w', encoding='utf8') as f:
+        # print('Changing "{old_string}" to "{new_string}" in {file_path}'.format(**locals()))
+        s = s.replace(old_string, new_string)
+        f.write(s)
 
 
 def replace_mod_files(path):
     # Replace http imgur and pastebin links to https
-    json_files = [pos_json for pos_json in os.listdir(path + "/Mods/Workshop") if pos_json.endswith('.json')]
+    json_files = [pos_json for pos_json in os.listdir(path) if pos_json.endswith('.json')]
 
     for file in json_files:
-        inplace_change(path + "/Mods/Workshop/" + file, "http://imgur.com", "https://imgur.com")
-        inplace_change(path + "/Mods/Workshop/" + file, "http://i.imgur.com", "https://i.imgur.com")
-        inplace_change(path + "/Mods/Workshop/" + file, "http://pastebin.com", "https://pastebin.com")
-
-    json_files = [pos_json for pos_json in os.listdir(path + "/Saves") if pos_json.endswith('.json')]
-
-    for file in json_files:
-        inplace_change(path + "/Saves/" + file, "http://imgur.com", "https://imgur.com")
-        inplace_change(path + "/Saves/" + file, "http://i.imgur.com", "https://i.imgur.com")
-        inplace_change(path + "/Saves/" + file, "http://pastebin.com", "https://pastebin.com")
+        inplace_change(path + file, "http://imgur.com", "https://imgur.com")
+        inplace_change(path + file, "http://i.imgur.com", "https://i.imgur.com")
+        inplace_change(path + file, "http://pastebin.com", "https://pastebin.com")
 
 
 def rename_downloaded_files(path):
-    path_images = path + "/Mods/Images/"
 
-    for filename in os.listdir(path_images):
-
-        dst = filename
-        dst = dst.replace("httpimgurcom", "httpsimgurcom")
-        dst = dst.replace("httppastebincom", "httpsiimgurcom")
-        dst = dst.replace("httpiimgurcom", "httpsiimgurcom")
-
-        src = path_images + filename
-        dst = path_images + dst
-
-        if src != dst:
-            print(src)
-            print(dst)
-            os.rename(src, dst)
-
-    path_models = path + "/Mods/Models/"
-
-    for filename in os.listdir(path_models):
+    for filename in os.listdir(path):
 
         dst = filename
         dst = dst.replace("httpimgurcom", "httpsimgurcom")
         dst = dst.replace("httppastebincom", "httpsiimgurcom")
         dst = dst.replace("httpiimgurcom", "httpsiimgurcom")
 
-        src = path_models + filename
-        dst = path_models + dst
+        src = path + filename
+        dst = path + dst
 
         if src != dst:
             print(src)
@@ -97,10 +68,13 @@ def rename_downloaded_files(path):
 
 if __name__ == "__main__":
 
-    path = get_path()
+    chosen_path = get_path()
 
-    do_backup(path)
+    do_backup(chosen_path + "/Mods/Workshop")
+    do_backup(chosen_path + "/Saves")
 
-    replace_mod_files(path)
+    replace_mod_files(chosen_path + "/Mods/Workshop/")
+    replace_mod_files(chosen_path + "/Saves/")
 
-    rename_downloaded_files(path)
+    rename_downloaded_files(chosen_path + "/Mods/Images/")
+    rename_downloaded_files(chosen_path + "/Mods/Models/")
