7bdf0c2ba7d89779fedafb4146a1ddc8ac0bc0ae
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2018-08-20 14:57:48 +0300

    added localization

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter import filedialog
 import os
 import shutil
 import sys, time, msvcrt
-
+import locale
 
 proxies = {
     "https": "http",
@@ -11,6 +11,35 @@ proxies = {
     "pastebin.com": "pastebin.seyahdoo.com",
     "cubeupload.com": "cubeupload.seyahdoo.com"
 }
+
+words = [
+    {
+        'en': "Getting root mods path",
+        'tr': "Mod kök dosyası bulunuyor"
+    },{
+        'en': "Backing up intial data",
+        'tr': "Orjinal dosyalar yedekleniyor"
+    },{
+        'en': "Proxying json mod files",
+        'tr': "Json mod dosyaları proxy'leriyle değiştiriliyor."
+    },{
+        'en': "Fixing previously downloaded Image and Model cache",
+        'tr': "Önceden indirilmiş Resimler ve Modellerin isimleri düzeltiliyor"
+    },{
+        'en': "DONE!",
+        'tr': "BİTTİ!"
+    },{
+        'en': "Press Enter to continue...",
+        'tr': "Çıkmak için Enter'a basınız..."
+    }
+]
+
+
+def get_localized_string(locale, string_no):
+    if locale == 'tr':
+        return words[string_no]['tr']
+    else:
+        return words[string_no]['en']
 
 
 def de_specialized(string):
@@ -82,7 +111,7 @@ def rename_downloaded_files(file_path):
                 os.rename(src, dst)
 
 
-def wait_enter_or_seconds( caption, timeout = 5):
+def wait_enter_or_seconds(caption, timeout = 5):
 
     start_time = time.time()
     sys.stdout.write('%s'%(caption))
@@ -104,23 +133,30 @@ def wait_enter_or_seconds( caption, timeout = 5):
 
 if __name__ == "__main__":
 
-    print("Getting root mods path")
-    root_path = get_root_path()
-    print("Got root mods path")
+    lang = locale.getdefaultlocale()[0].split('_')[0]
 
-    print("Backing up intial data")
+    # Getting root mods path
+    print(get_localized_string(lang,0))
+    root_path = get_root_path()
+
+    # Backing up intial data
+    print(get_localized_string(lang,1))
     do_backup(root_path + "/Mods/Workshop")
     do_backup(root_path + "/Saves")
 
-    print("Turkeyifying json mod files")
+    # Proxying json mod files
+    print(get_localized_string(lang,2))
     replace_mod_files(root_path + "/Mods/Workshop/")
     replace_mod_files(root_path + "/Saves/")
 
-    print("Fixing previously downloaded Image and Model cache")
+    # Fixing previously downloaded Image and Model cache
+    print(get_localized_string(lang,3))
     rename_downloaded_files(root_path + "/Mods/Images/")
     rename_downloaded_files(root_path + "/Mods/Models/")
 
-    print("DONE!")
+    # DONE!
+    print(get_localized_string(lang,4))
 
-    wait_enter_or_seconds('Press Enter to continue...', 3)
+    # Press Enter to continue...
+    wait_enter_or_seconds(get_localized_string(lang,5), 3)
 
