e3569bb4d34e4e7669b4402ad53999d6481c6add
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2018-08-20 18:14:06 +0300

    generalized proxy code

--- proxy.py
+++ proxy.py
@@ -4,18 +4,19 @@ from flask import request
 from requests import get
 
 app = Flask(__name__)
-SITE_NAME = 'http://pastebin.com'
 
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def proxy(path):
-    url = SITE_NAME + request.full_path
-    c = get(url).content
-    r = Response(response=c, status=200)
-    r.headers["Content-Type"] = "text/plain; charset=utf-8"
+    written_host = request.host_url
+    real_site = written_host.replace(".seyahdoo","")
+    url = real_site + request.full_path[1:]
+    c = get(url)
+    r = Response(response=c.content, status=200)
+    r.headers["Content-Type"] = c.headers["Content-Type"]
     return r
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=80)
