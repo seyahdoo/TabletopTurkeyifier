fcaf407802b448e1df89ad6d4b731cf77ce9dad3
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2018-08-24 12:46:17 +0300

    Fixed backup error at saying backup not found

--- backup.py
+++ backup.py
@@ -6,6 +6,6 @@ from localization import get_localized_string
 
 def do_backup_folder(file_path):
     if not os.path.isdir(file_path + "BACKUP"):
-        print(get_localized_string("say_no_backup_found") + file_path + "BACKUP")
+        print(get_localized_string("no_backup_found") + file_path + "BACKUP")
         shutil.copytree(file_path, file_path + "BACKUP")
     return
