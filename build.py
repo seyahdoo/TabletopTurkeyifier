6a8d99b93292cea3b91e56e5d6e34eadc39917ab
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2018-08-28 00:20:42 +0300

    Auto rename distribution file

--- build.py
+++ build.py
@@ -6,11 +6,12 @@ s = ""
 with open("version_file.txt", 'r', encoding='utf8', errors='ignore') as f:
     s = f.read()
 
-version = version + ".0"
-s = s.replace("x.x.x.x", version)
 
-version = version.replace(".",",")
-s = s.replace("x,x,x,x", version)
+version_edited = version + ".0"
+s = s.replace("x.x.x.x", version_edited)
+
+version_edited = version_edited.replace(".", ",")
+s = s.replace("x,x,x,x", version_edited)
 
 with open("version_file_created.txt", 'w', encoding='utf8') as f:
     f.write(s)
@@ -19,7 +20,11 @@ os.system("pyinstaller --onefile main.py --version-file=\"version_file_created.t
 
 os.remove("version_file_created.txt")
 
-if os.path.isfile("./dist/tabletop-turkeyifier.exe"):
-    os.remove("./dist/tabletop-turkeyifier.exe")
+filename = "./dist/tabletop-turkeyifier-"
+filename += version
+filename += ".exe"
 
-os.rename("./dist/main.exe","./dist/tabletop-turkeyifier.exe")
+if os.path.isfile(filename):
+    os.remove(filename)
+
+os.rename("./dist/main.exe", filename)
