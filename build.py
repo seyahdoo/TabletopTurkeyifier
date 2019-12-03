53c03e08f0a498cc9448cc23a458bf66c796c69d
Seyyid Ahmed Doğan <contact@seyahdoo.com>
2019-12-03 09:49:48 +0300

    can build with tag names

--- build.py
+++ build.py
@@ -3,22 +3,18 @@ import os
 
 s = ""
 
-with open("version_file.txt", 'r', encoding='utf8', errors='ignore') as f:
+with open("build_properties_template.txt", 'r', encoding='utf8', errors='ignore') as f:
     s = f.read()
 
-
-version_edited = version + ".0"
+version_edited = version
 s = s.replace("x.x.x.x", version_edited)
 
-version_edited = version_edited.replace(".", ",")
-s = s.replace("x,x,x,x", version_edited)
-
-with open("version_file_created.txt", 'w', encoding='utf8') as f:
+with open("tmp_build_properties.txt", 'w', encoding='utf8') as f:
     f.write(s)
 
-os.system("pyinstaller --onefile main.py --version-file=\"version_file_created.txt\"")
+os.system("pyinstaller --onefile main.py --version-file=\"tmp_build_properties.txt\"")
 
-os.remove("version_file_created.txt")
+os.remove("tmp_build_properties.txt")
 
 filename = "./dist/tabletop-turkeyifier-"
 filename += version
