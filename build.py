2c3cdf5eb4c0683ebc1eaa2b749182f9256296d1
Seyyid Ahmed Doğan <contact@seyahdoo.com>
2019-12-04 19:29:22 +0300

    fix(build.py): changed the version file work logic

--- build.py
+++ build.py
@@ -1,24 +1,25 @@
-from version import version
 import os
+import subprocess
 
-s = ""
+cmd = subprocess.run(["git", "describe", "--abbrev=0"], stdout=subprocess.PIPE)
+version: str = cmd.stdout.decode("utf-8")
+version = version.strip()
 
+with open("version.py", "w", encoding="utf8", errors="ignore") as f:
+    f.write("version = \"{}\"".format(version))
+
+properties_file_content = ""
 with open("build_properties_template.txt", 'r', encoding='utf8', errors='ignore') as f:
-    s = f.read()
-
-version_edited = version
-s = s.replace("x.x.x.x", version_edited)
-
+    properties_file_content = f.read()
+properties_file_content = properties_file_content.replace("x.x.x.x", version)
 with open("tmp_build_properties.txt", 'w', encoding='utf8') as f:
-    f.write(s)
+    f.write(properties_file_content)
 
 os.system("pyinstaller --onefile main.py --version-file=\"tmp_build_properties.txt\"")
 
 os.remove("tmp_build_properties.txt")
 
 filename = "./dist/tabletop-turkeyifier.exe"
-
 if os.path.isfile(filename):
     os.remove(filename)
-
 os.rename("./dist/main.exe", filename)
