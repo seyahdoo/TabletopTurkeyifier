0a84e8cf8a2c02d75852f95a5bebd94209c8182b
Seyyid Ahmed Doğan <contact@seyahdoo.com>
2019-12-04 22:37:18 +0300

    chore(tag): tag fix

--- build.py
+++ build.py
@@ -9,6 +9,8 @@ version = version.strip()
 with open("version.py", "w", encoding="utf8", errors="ignore") as f:
     f.write("version = \"{}\"".format(version))
 
+print("::set-output name=version::{}".format(version))
+
 properties_file_content = ""
 with open("build_properties_template.txt", 'r', encoding='utf8', errors='ignore') as f:
     properties_file_content = f.read()
@@ -25,20 +27,22 @@ if os.path.isfile(filename):
     os.remove(filename)
 os.rename("./dist/main.exe", filename)
 
-GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 
-g = Github(GITHUB_TOKEN)
 
-cmd = subprocess.run(["git", "config", "--get", "remote.origin.url"], stdout=subprocess.PIPE)
-repo_url: str = cmd.stdout.decode("utf-8")
-repo_url = repo_url.strip()
-
-repo = g.get_repo(repo_url)
-
-release = repo.create_git_release(
-    version,
-    "Tabletop Turkeyifier {}".format(version),
-    "Tabletop Turkeyifier {}".format(version))
+# GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+# g = Github(GITHUB_TOKEN)
+#
+# cmd = subprocess.run(["git", "config", "--get", "remote.origin.url"], stdout=subprocess.PIPE)
+# repo_url: str = cmd.stdout.decode("utf-8")
+# repo_url = repo_url.strip()
+# print(repo_url)
+#
+# repo = g.get_repo(repo_url)
+#
+# release = repo.create_git_release(
+#     version,
+#     "Tabletop Turkeyifier {}".format(version),
+#     "Tabletop Turkeyifier {}".format(version))
 
 
 
