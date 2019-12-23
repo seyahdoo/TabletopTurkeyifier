343f042e11fd55d3e00b4713f2f5158d9b7bcd59
Seyyid Ahmed Doğan <contact@seyahdoo.com>
2019-12-23 21:21:43 +0300

    fix(backup): backup folder fix

--- backup.py
+++ backup.py
@@ -5,7 +5,7 @@ from localization import get_localized_string
 
 
 def do_backup_folder(file_path):
-    backup_path = os.path.join(file_path, "..", "BACKUP")
+    backup_path = file_path + "BACKUP"
     if not os.path.isdir(backup_path):
         print(get_localized_string("no_backup_found") + backup_path)
         shutil.copytree(file_path, backup_path)
