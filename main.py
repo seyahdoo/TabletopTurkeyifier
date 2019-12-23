d6e6735ebf342321338ea0899a1e295221528904
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2019-12-23 03:02:05 +0300

    fix(paths): edge case, mods or images can be at steam library folder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from sys import exit
 from localization import get_localized_string, print_localized
-from path import get_mods_root_path
+from path import get_paths, get_documents_root
 from backup import do_backup_folder
 from proxify import Proxify
 from link import sym_link_already_downloaded_files
@@ -29,42 +29,28 @@ if __name__ == "__main__":
 
     # Getting root mods path
     print_localized("find_root")
-    root_path = get_mods_root_path()
-
-    mods_list = [
-        root_path + "/Mods/Workshop/",
-        root_path + "/Saves/",
-    ]
-
-    asset_folder_list = [
-        root_path + "/Mods/Assetbundles/",
-        root_path + "/Mods/Audio/",
-        root_path + "/Mods/Images/",
-        root_path + "/Mods/Images Raw/",
-        root_path + "/Mods/Models/",
-        root_path + "/Mods/Models Raw/",
-        root_path + "/Mods/PDF/",
-        root_path + "/Mods/Text/",
-    ]
+    mods_list, asset_folder_list = get_paths()
 
     # Backing up intial data
     print_localized("backup")
-    do_backup_folder(root_path + "/Mods/Workshop")
-    do_backup_folder(root_path + "/Saves")
+    for mod_path in mods_list:
+        do_backup_folder(mod_path)
 
     p = Proxify()
 
+    history_location = "{}\\TurkeyifierHistory.json".format(get_documents_root())
+
     # Proxying json mod files
-    p.load_proxy_history(root_path + "/TurkeyifierHistory.json")
+    p.load_proxy_history(history_location)
 
     if just_updated:
         # Revert old proxies
         p.proxify_mod_files_in_folder_list(mods_list, True)
-        p.reset_proxy_history(root_path + "/TurkeyifierHistory.json")
+        p.reset_proxy_history(history_location)
 
     # Do proxy calculations and save
     p.proxify_mod_files_in_folder_list(mods_list, False)
-    p.save_proxy_history(root_path + "/TurkeyifierHistory.json")
+    p.save_proxy_history(history_location)
 
     # Fixing previously downloaded Image and Model cache
     print_localized("fixing_links")
