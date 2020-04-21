4e38b9bd8f76ff278b4b1a822e4988a43fdbe866
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2020-04-21 12:37:43 +0300

    fix(link.py): fixed linking error

--- link.py
+++ link.py
@@ -10,38 +10,45 @@ def sym_link_already_downloaded_files(p, folder_list):
         for filename in os.listdir(folder_path):
 
             # if this is not a link, and if this is a proxified file
-            if (not os.path.islink(folder_path + filename)) and (p.is_proxy_or_original(filename) != "irrelevant"):
+            file_path = os.path.join(folder_path, filename)
+            if (not os.path.islink(file_path)) and (p.is_proxy_or_original(filename) != "irrelevant"):
 
                 original_name = None
                 proxy_name = None
+                original_path = None
+                proxy_path = None
 
                 # if proxy, delete original and rename this to be original
                 if p.is_proxy_or_original(filename) == "proxy":
                     proxy_name = filename
+                    proxy_path = os.path.join(folder_path, proxy_name)
                     original_name = p.get_original_from_proxy_non_special(filename)
-                    if os.path.isfile(folder_path + original_name):
+                    original_path = os.path.join(folder_path, original_name)
+                    if os.path.isfile(original_path):
                         # print("removing" + folder_path + original_name)
-                        os.remove(folder_path + original_name)
+                        os.remove(original_path)
                     # print("renaming" + folder_path + proxy_name + " to " + folder_path + original_name)
-                    os.rename(folder_path + proxy_name, folder_path + original_name)
+                    os.rename(proxy_path, original_path)
 
                 # if original, delete proxy,
                 elif p.is_proxy_or_original(filename) == "original":
                     original_name = filename
+                    original_path = os.path.join(folder_path, original_name)
                     proxy_name = p.get_proxy_from_original_non_special(filename)
-                    if os.path.isfile(folder_path + proxy_name):
+                    proxy_path = os.path.join(folder_path, proxy_name)
+                    if os.path.isfile(proxy_path):
                         # print("removing" + folder_path + proxy_name)
-                        os.remove(folder_path + proxy_name)
+                        os.remove(proxy_path)
 
                 # now only original file exists
                 # link original to proxy
                 # so the game wont re-download it. (for host that will not be using this program)
                 # print("linking" + folder_path + original_name + " to " + folder_path + proxy_name)
-                os.symlink(folder_path + original_name, folder_path + proxy_name)
+                os.symlink(original_path, proxy_path)
 
             # if this is an invalid link, delete it
-            elif os.path.islink(folder_path + filename):
-                if not os.path.exists(os.readlink(folder_path + filename)):
-                    print("invalid link detected, deleting " + folder_path + filename)
-                    os.remove(folder_path + filename)
+            elif os.path.islink(file_path):
+                if not os.path.exists(os.readlink(file_path)):
+                    print("invalid link detected, deleting " + file_path)
+                    os.remove(file_path)
     return
