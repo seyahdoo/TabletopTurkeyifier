d7a320c0c5e83896d89af9c1cafcfd83e3f84698
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2021-05-12 10:38:38 +0300

    fix(proxify): raw php links not working fixed, thanks to yigit-alp

--- proxify.py
+++ proxify.py
@@ -8,10 +8,10 @@ from localization import get_localized_string, print_localized
 class Proxify:
 
     def __init__(self):
-        # cubeupload was banned too but they seem to be unbanned
         self.proxies = {
             "https:": "http:",
-            "http://pastebin.com": "http://pastebinp.com",
+            "http://pastebin.com/raw.php?i=": "http://pastebinp.com/raw/",
+            "http://pastebin.com/raw/": "http://pastebinp.com/raw/",
             "http://imgur.com": "http://0imgur.com",
             "http://i.imgur.com": "http://i.0imgur.com",
         }
